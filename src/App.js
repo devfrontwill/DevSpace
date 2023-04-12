d31eb80a94882cfbb5aa848f450ff5ac14f11aea
Wiliam Roza <devfrontwill@gmail.com>
2023-04-12 01:34:44 -0300

    criado novos compononentes estaticos e estilizados com sass

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
-
 import './styles/estilosGlobais.scss'
 import PaginaInicial from "./paginas/PaginaInicial";
 
-
-function App() {
-  return (
-    <PaginaInicial />
-  );
+export default function App() {
+  return <PaginaInicial/>;
 }
-
-export default App;
--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,7 +1,23 @@
+import banner from "./banner.png";
 import Cabecalho from "../../componentes/Cabecalho";
+import Menu from "../../componentes/Menu";
+import styles from "./PaginaInicial.module.scss";
+import Rodape from "../../componentes/Rodape";
 
-export default function PaginaInicial () {
-
-    return <Cabecalho />
-    
+export default function PaginaInicial() {
+  return (
+    <>
+      <Cabecalho />
+      <main>
+        <section className={styles.principal}>
+          <Menu />
+          <div className={styles.principal__imagem}>
+            <h1>A galeria mais completa do espaço</h1>
+            <img src={banner} alt="A imagem da terra vista do espaço" />
+          </div>
+        </section>
+      </main>
+      <Rodape />
+    </>
+  );
 }
